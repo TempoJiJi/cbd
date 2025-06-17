a1c3fa785d2abec579866c9a3a1c9b4df0a4e51f
TempoJiJi <tempo_jiji94@hotmail.com>
2025-06-18 01:01:59 +0800

    Updated background.php

--- background.php
+++ background.php
@@ -1,4 +1,4 @@
-<div class="geometric-layer">
+    <div class="geometric-layer">
       <div class="shape"></div>
       <div class="shape"></div>
       <div class="shape"></div>
@@ -17,28 +17,4 @@
       <div class="particle"></div>
       <div class="particle"></div>
       <div class="particle"></div>
-      <div class="particle"></div>
-      <div class="particle"></div>
-    </div>
-
-    <!-- Elegant Lines -->
-    <div class="elegant-lines">
-      <div class="line"></div>
-      <div class="line"></div>
-      <div class="line"></div>
-      <div class="line"></div>
-      <div class="line"></div>
-    </div>
-
-    <!-- Ambient Glow -->
-    <div class="ambient-glow">
-      <div class="glow"></div>
-      <div class="glow"></div>
-      <div class="glow"></div>
-    </div>
-
-    <!-- Typography Elements -->
-    <div class="typography-layer">
-      <div class="text-element">CODE</div>
-      <div class="text-element">TECH</div>
     </div>
